00cc869ed4fb76ccdfbb6bfc8e2e01a0cbbebe3e
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-07-19 20:51:56 +0000

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
+import Button from "./components/Button";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <ButtonGradient />
+      </div>
+    </>
+  );
 }
 
 export default App;
