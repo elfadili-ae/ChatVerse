e0553fc157efb0957c50c0029118da132bee90b0
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-07-21 20:51:47 +0000

    benefits section added

---
+++ src/components/Collaboration.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Section from "./Section";
+
+const Collaboration = () => {
+  return (
+    <Section crosses>
+      <div className="container lg:flex">
+        <div className="max-w-[25rem]">
+          <h2>AI Chat</h2>
+        </div>
+      </div>
+      ;
+    </Section>
+  );
+};
+
+export default Collaboration;
