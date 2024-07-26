a83bad329e43a0bf3cd6b1d8995aab3a2c01941f
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-07-26 15:10:34 +0000

    added pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </>
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Section from "./Section";
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
+
+const Pricing = () => {
+  return (
+    <Section id="pricing" className="overflow-hidden">
+      <div className="relative z-2 m-8 md:m-16">
+        <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
+          <img
+            src={smallSphere}
+            className="relative z-1"
+            width={255}
+            height={255}
+            alt="Sphere"
+          />
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <img
+              src={stars}
+              className="w-ull"
+              width={950}
+              height={400}
+              alt="stars"
+            />
+          </div>
+        </div>
+        <Heading
+          tag="Get started with ChatVerse"
+          title="Pay once, use forever"
+        />
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Pricing;
